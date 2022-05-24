242feb56be64479706bb9aa6feb8d43d12dc71f9
galaxyuliana <yulianashin613@gmail.com>
2022-05-25 01:16:43 +0900

    Added ReactivityFundamentals

---
+++ first-vue-project/src/components/Exercise/02/Exercise02.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ReactivityFundamentals from './ReactivityFundamentals.vue'
+</script>
+
+<template>
+    <div>
+        <h2>Exercise 02</h2>
+        <h4>ReactivityFundamentals</h4>
+        <ReactivityFundamentals />
+    </div>
+</template>
---
+++ first-vue-project/src/components/Exercise/02/ReactivityFundamentals.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+
+  mounted() {
+    console.log(this.count) // => 1
+
+    this.count = 2
+  }
+}
+</script>
+
+<template>
+  Count is: {{ count }}
+</template>
