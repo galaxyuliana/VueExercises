89cee65b32b20b809a9f390acb78493798c8130c
galaxyuliana <yulianashin613@gmail.com>
2022-05-25 21:20:39 +0900

    Added ReactiveProxyVsOriginal

--- first-vue-project/src/components/Exercise/02/Exercise02.vue
+++ first-vue-project/src/components/Exercise/02/Exercise02.vue
@@ -1,11 +1,14 @@
 <script setup>
 import ReactivityFundamentals from './ReactivityFundamentals.vue'
+import ReactiveProxyVsOriginal from './ReactiveProxyVsOriginal.vue'
 </script>
 
 <template>
     <div>
         <h2>Exercise 02</h2>
-        <h4>ReactivityFundamentals</h4>
+        <h4>Reactivity Fundamentals</h4>
         <ReactivityFundamentals />
+        <h4>Reactive Proxy Vs. Original</h4>
+        <ReactiveProxyVsOriginal />
     </div>
 </template>
