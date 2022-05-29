c2cd522b667667800ed51816dc3caf672ed48e40
galaxyuliana <yulianashin613@gmail.com>
2022-05-30 00:25:23 +0900

    Added BindingToObjects.vue

---
+++ first-vue-project/src/components/Exercise/04/BindingToObjects.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        isActive: true,
+        hasError: false
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div
+            class="static"
+            :class="{ active: isActive, 'text-danger': hasError }"
+        ></div>
+    </div>
+</template>
--- first-vue-project/src/components/Exercise/04/Exercise04.vue
+++ first-vue-project/src/components/Exercise/04/Exercise04.vue
@@ -1,7 +1,11 @@
 <script setup>
+import BindingToObjects from './BindingToObjects.vue'
 </script>
 
 <template>
     <div>
+        <h2>Exercise 04</h2>
+        <h4>Binding to Objects</h4>
+        <BindingToObjects />
     </div>
 </template>
