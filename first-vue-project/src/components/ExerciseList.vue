eabc28e59f30ee3c132983d41dfbb05279a1ed81
galaxyuliana <yulianashin613@gmail.com>
2022-05-30 00:10:00 +0900

    Added Exercise04

--- first-vue-project/src/components/ExerciseList.vue
+++ first-vue-project/src/components/ExerciseList.vue
@@ -2,12 +2,16 @@
 import Exercise01 from './Exercise/01/Exercise01.vue'
 import Exercise02 from './Exercise/02/Exercise02.vue'
 import Exercise03 from './Exercise/03/Exercise03.vue'
+import Exercise04 from './Exercise/04/Exercise04.vue'
 </script>
 
 <template>
-    <router-link class="nav-link" to="/01">Exercies 01</router-link>
-    <router-link class="nav-link" to="/02">Exercies 02</router-link>
-    <router-link class="nav-link" to="/03">Exercies 03</router-link>
+    <div>
+        <router-link class="nav-link" to="/01">Exercies 01</router-link>
+        <router-link class="nav-link" to="/02">Exercies 02</router-link>
+        <router-link class="nav-link" to="/03">Exercies 03</router-link>
+        <router-link class="nav-link" to="/04">Exercies 04</router-link>
+    </div>
 </template>
 
 <style>
--- first-vue-project/src/router.js
+++ first-vue-project/src/router.js
@@ -3,6 +3,7 @@ import ExerciseList from './components/ExerciseList.vue'
 import Exercise01 from './components/Exercise/01/Exercise01.vue'
 import Exercise02 from './components/Exercise/02/Exercise02.vue'
 import Exercise03 from './components/Exercise/03/Exercise03.vue'
+import Exercise04 from './components/Exercise/04/Exercise04.vue'
 
 const routes = [{
     path: '/',
@@ -23,6 +24,11 @@ const routes = [{
     path: '/03',
     name: 'Exercise03',
     component: Exercise03
+  },
+  {
+    path: '/04',
+    name: 'Exercise04',
+    component: Exercise04
   }
 ]
 
