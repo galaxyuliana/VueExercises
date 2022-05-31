e376b3d20873463d712aedcbb129d808d8ea452c
galaxyuliana <yulianashin613@gmail.com>
2022-05-31 22:14:08 +0900

    Added BindingToObjectComputed

--- first-vue-project/src/components/Exercise/04/Exercise04.vue
+++ first-vue-project/src/components/Exercise/04/Exercise04.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BindingToObjects from './BindingToObjects.vue'
 import BindingToObjectNotInline from './BindingToObjectNotInline.vue'
+import BindingToObjectComputed from './BindingToObjectComputed.vue'
 </script>
 
 <template>
@@ -10,5 +11,7 @@ import BindingToObjectNotInline from './BindingToObjectNotInline.vue'
         <BindingToObjects />
         <h4>Binding to Objects2</h4>
         <BindingToObjectNotInline />
+        <h4>Binding to Objects using computed</h4>
+        <BindingToObjectComputed />
     </div>
 </template>
