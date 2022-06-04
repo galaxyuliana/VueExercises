c512f18af92ca55d98db14b0e9325c3e9dfeb39e
galaxyuliana <yulianashin613@gmail.com>
2022-06-04 23:30:59 +0900

    Added Exercise05

--- first-vue-project/src/components/ExerciseList.vue
+++ first-vue-project/src/components/ExerciseList.vue
@@ -3,6 +3,7 @@ import Exercise01 from './Exercise/01/Exercise01.vue'
 import Exercise02 from './Exercise/02/Exercise02.vue'
 import Exercise03 from './Exercise/03/Exercise03.vue'
 import Exercise04 from './Exercise/04/Exercise04.vue'
+import Exercise05 from './Exercise/05/Exercise05.vue'
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import Exercise04 from './Exercise/04/Exercise04.vue'
         <router-link class="nav-link" to="/02">Exercies 02</router-link>
         <router-link class="nav-link" to="/03">Exercies 03</router-link>
         <router-link class="nav-link" to="/04">Exercies 04</router-link>
+        <router-link class="nav-link" to="/05">Exercies 05</router-link>
     </div>
 </template>
 
