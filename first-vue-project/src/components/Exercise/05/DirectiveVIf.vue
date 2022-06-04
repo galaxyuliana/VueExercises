d921168f45781ac338bedb0e20e004976f4b4906
galaxyuliana <yulianashin613@gmail.com>
2022-06-04 23:46:57 +0900

    Added DirectiveVIf

---
+++ first-vue-project/src/components/Exercise/05/DirectiveVIf.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+        awesome: true,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h1 v-if="awesome">Vue is awesome!</h1>
+    </div>
+</template>
---
+++ first-vue-project/src/components/Exercise/05/Exercise05.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DirectiveVIf from './DirectiveVIf.vue'
+</script>
+
+<template>
+    <div>
+        <h2>Exercise 05</h2>
+        <h4>Directive v-if</h4>
+        <DirectiveVIf />
+    </div>
+</template>
