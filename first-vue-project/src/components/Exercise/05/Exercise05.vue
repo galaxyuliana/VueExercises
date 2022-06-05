495dc999555bb9b9e42dcdf993d02ee1451b7ccc
galaxyuliana <yulianashin613@gmail.com>
2022-06-05 21:05:35 +0900

    Added DirectiveVElseIf

--- first-vue-project/src/components/Exercise/05/Exercise05.vue
+++ first-vue-project/src/components/Exercise/05/Exercise05.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DirectiveVIf from './DirectiveVIf.vue'
 import DirectiveVElse from './DirectiveVElse.vue'
+import DirectiveVElseIf from './DirectiveVElseIf.vue'
 </script>
 
 <template>
@@ -10,5 +11,7 @@ import DirectiveVElse from './DirectiveVElse.vue'
         <DirectiveVIf />
         <h4>Directive v-else</h4>
         <DirectiveVElse />
+        <h4>Directive v-else-if</h4>
+        <DirectiveVElseIf />
     </div>
 </template>
