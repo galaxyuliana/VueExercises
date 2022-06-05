80ecec61f95253928a09f899360518e7e33dc69d
galaxyuliana <yulianashin613@gmail.com>
2022-06-05 20:57:45 +0900

    Added DirectiveVElse

---
+++ first-vue-project/src/components/Exercise/05/DirectiveVElse.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+        awesome: true,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="awesome = !awesome">Toggle</button>
+
+        <h1 v-if="awesome">Vue is awesome!</h1>
+        <h1 v-else>Oh no 😢</h1>
+    </div>
+</template>
--- first-vue-project/src/components/Exercise/05/Exercise05.vue
+++ first-vue-project/src/components/Exercise/05/Exercise05.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DirectiveVIf from './DirectiveVIf.vue'
+import DirectiveVElse from './DirectiveVElse.vue'
 </script>
 
 <template>
@@ -7,5 +8,7 @@ import DirectiveVIf from './DirectiveVIf.vue'
         <h2>Exercise 05</h2>
         <h4>Directive v-if</h4>
         <DirectiveVIf />
+        <h4>Directive v-else</h4>
+        <DirectiveVElse />
     </div>
 </template>
