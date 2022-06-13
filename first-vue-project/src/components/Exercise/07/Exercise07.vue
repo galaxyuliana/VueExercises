f993f2dd23fe66486c4cf06588f054aaf09779f8
galaxyuliana <yulianashin613@gmail.com>
2022-06-13 22:58:57 +0900

    Added DirectiveVOn

--- first-vue-project/src/components/Exercise/07/Exercise07.vue
+++ first-vue-project/src/components/Exercise/07/Exercise07.vue
@@ -1,5 +1,11 @@
+<script setup>
+import DirectiveVOn from './DirectiveVOn.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 07</h2>
+        <h4>Directive v-on</h4>
+        <DirectiveVOn />
     </div>
 </template>
