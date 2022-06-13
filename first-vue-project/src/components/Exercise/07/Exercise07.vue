4d76275a026b492fd3c6d86f7029afd96912c23f
galaxyuliana <yulianashin613@gmail.com>
2022-06-14 00:07:13 +0900

    Added DirectiveVOnCallMethodInline

--- first-vue-project/src/components/Exercise/07/Exercise07.vue
+++ first-vue-project/src/components/Exercise/07/Exercise07.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DirectiveVOn from './DirectiveVOn.vue'
 import DirectiveVOnMethodHandlers from './DirectiveVOnMethodHandlers.vue'
+import DirectiveVOnCallMethodInline from './DirectiveVOnCallMethodInline.vue'
 </script>
 
 <template>
@@ -10,5 +11,7 @@ import DirectiveVOnMethodHandlers from './DirectiveVOnMethodHandlers.vue'
         <DirectiveVOn />
         <h4>Directive v-on with method handlers</h4>
         <DirectiveVOnMethodHandlers />
+        <h4>Calling Methods in Inline Handlers</h4>
+        <DirectiveVOnCallMethodInline />
     </div>
 </template>
