2ba05e1d55960de88a9d782be4104572c2f69550
galaxyuliana <yulianashin613@gmail.com>
2022-06-13 23:10:39 +0900

    Added DirectiveVOnMethodHandlers

--- first-vue-project/src/components/Exercise/07/Exercise07.vue
+++ first-vue-project/src/components/Exercise/07/Exercise07.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DirectiveVOn from './DirectiveVOn.vue'
+import DirectiveVOnMethodHandlers from './DirectiveVOnMethodHandlers.vue'
 </script>
 
 <template>
@@ -7,5 +8,7 @@ import DirectiveVOn from './DirectiveVOn.vue'
         <h2>Exercise 07</h2>
         <h4>Directive v-on</h4>
         <DirectiveVOn />
+        <h4>Directive v-on with method handlers</h4>
+        <DirectiveVOnMethodHandlers />
     </div>
 </template>
