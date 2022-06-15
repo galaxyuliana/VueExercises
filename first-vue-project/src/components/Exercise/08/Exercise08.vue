ddc1b2084060153a54211eaeda2cfb08cddcbc5d
galaxyuliana <yulianashin613@gmail.com>
2022-06-16 01:01:44 +0900

    Added FormInputBindings

--- first-vue-project/src/components/Exercise/08/Exercise08.vue
+++ first-vue-project/src/components/Exercise/08/Exercise08.vue
@@ -1,5 +1,11 @@
+<script setup>
+import FormInputBindings from './FormInputBindings.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 08</h2>
+        <h4>Form Input Bindings</h4>
+        <FormInputBindings />
     </div>
 </template>
