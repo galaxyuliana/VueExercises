ab3371c973f08eddfdf45dd7dafcfbaa2536d3ad
galaxyuliana <yulianashin613@gmail.com>
2022-06-21 02:28:05 +0900

    Added LifecycleHooks

--- first-vue-project/src/components/Exercise/09/Exercise09.vue
+++ first-vue-project/src/components/Exercise/09/Exercise09.vue
@@ -1,5 +1,11 @@
+<script setup>
+import LifecycleHooks from './LifecycleHooks.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 09</h2>
+        <h4>Lifecycle Hooks</h4>
+        <LifecycleHooks />
     </div>
 </template>
