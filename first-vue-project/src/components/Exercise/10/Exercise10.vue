3f1276950893662538f15b189183776d9ec1db17
galaxyuliana <yulianashin613@gmail.com>
2022-06-22 02:38:52 +0900

    Added BasicExample

--- first-vue-project/src/components/Exercise/10/Exercise10.vue
+++ first-vue-project/src/components/Exercise/10/Exercise10.vue
@@ -1,5 +1,11 @@
+<script setup>
+import BasicExample from './BasicExample.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 10</h2>
+        <h4>Basic Example</h4>
+        <BasicExample />
     </div>
 </template>
