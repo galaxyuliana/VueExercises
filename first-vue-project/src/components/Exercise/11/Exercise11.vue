e9b6450bdd7c51d2e61480163ab7a679f78f058c
galaxyuliana <yulianashin613@gmail.com>
2022-06-29 23:31:09 -0700

    Added RefsInsideVFor

--- first-vue-project/src/components/Exercise/11/Exercise11.vue
+++ first-vue-project/src/components/Exercise/11/Exercise11.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AccessingTheRefs from './AccessingTheRefs.vue'
+import RefsInsideVFor from './RefsInsideVFor.vue'
 </script>
 
 <template>
@@ -7,5 +8,7 @@ import AccessingTheRefs from './AccessingTheRefs.vue'
         <h2>Exercise 11</h2>
         <h4>Accessing The Refs</h4>
         <AccessingTheRefs />
+        <h4>Refs inside v-for</h4>
+        <RefsInsideVFor />
     </div>
 </template>
