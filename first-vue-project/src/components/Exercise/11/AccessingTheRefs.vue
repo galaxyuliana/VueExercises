6601c6c5472e3b7a40818c0ac40e3aed795de4ed
galaxyuliana <yulianashin613@gmail.com>
2022-06-29 23:25:29 -0700

    Added AccessingTheRefs

---
+++ first-vue-project/src/components/Exercise/11/AccessingTheRefs.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.input.focus()
+  }
+}
+</script>
+
+<template>
+  <input ref="input" />
+</template>
--- first-vue-project/src/components/Exercise/11/Exercise11.vue
+++ first-vue-project/src/components/Exercise/11/Exercise11.vue
@@ -1,5 +1,11 @@
+<script setup>
+import AccessingTheRefs from './AccessingTheRefs.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 11</h2>
+        <h4>Accessing The Refs</h4>
+        <AccessingTheRefs />
     </div>
 </template>
