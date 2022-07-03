3b3726eca900f66c29920e02e3424b589a60f833
galaxyuliana <yulianashin613@gmail.com>
2022-07-02 22:12:05 -0700

    Added DefiningComponent

--- first-vue-project/src/components/Exercise/12/Exercise12.vue
+++ first-vue-project/src/components/Exercise/12/Exercise12.vue
@@ -1,5 +1,11 @@
+<script setup>
+import DefiningComponent from './DefiningComponent.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 12</h2>
+        <h4>Defining a Component</h4>
+        <DefiningComponent />
     </div>
 </template>
