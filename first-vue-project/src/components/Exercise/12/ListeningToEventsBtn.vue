38eadf29baaa456ee5f288c49e764ae2a0c3c37b
galaxyuliana <yulianashin613@gmail.com>
2022-07-07 18:19:19 -0700

    Added ListeningToEventsBtn, BlogPostBtn

---
+++ first-vue-project/src/components/Exercise/12/ListeningToEventsBtn.vue
@@ -0,0 +1,32 @@
+<script>
+import BlogPostBtn from './BlogPostBtn.vue'
+
+export default {
+  components: {
+    BlogPostBtn
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' }
+      ],
+      postFontSize: 3
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <div :style="{ fontSize: postFontSize + 'em' }">
+            <BlogPostBtn
+                v-for="post in posts"
+                :key="post.id"
+                :title="post.title"
+                @enlarge-text="postFontSize += 0.1"
+            />
+        </div>
+    </div>
+</template>
