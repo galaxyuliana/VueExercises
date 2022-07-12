7cfedeb948ee72eb5f5cddbc2ba269ea6d8ba445
galaxyuliana <yulianashin613@gmail.com>
2022-07-11 22:22:20 -0700

    Added GlobalRegistration

--- first-vue-project/src/components/Exercise/13/Exercise13.vue
+++ first-vue-project/src/components/Exercise/13/Exercise13.vue
@@ -1,9 +1,11 @@
+<script setup>
+import GlobalRegistration from './GlobalRegistration.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 13</h2>
         <h4>Global Registration</h4>
-        <ComponentA/>
-        <ComponentB/>
-        <ComponentC/>
+        <GlobalRegistration/>
     </div>
 </template>
