2a90a7335af68f34e5f799511159c93a40170783
galaxyuliana <yulianashin613@gmail.com>
2022-07-11 22:21:14 -0700

    Added ComponentA, B, C and globally registered

--- first-vue-project/src/components/Exercise/13/Exercise13.vue
+++ first-vue-project/src/components/Exercise/13/Exercise13.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
         <h2>Exercise 13</h2>
+        <h4>Global Registration</h4>
+        <ComponentA/>
+        <ComponentB/>
+        <ComponentC/>
     </div>
 </template>
--- first-vue-project/src/main.js
+++ first-vue-project/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import ComponentA from './components/Exercise/13/ComponentA.vue'
+import ComponentB from './components/Exercise/13/ComponentB.vue'
+import ComponentC from './components/Exercise/13/ComponentC.vue'
 
-const app = createApp(App);
+const app = createApp(App)
+.component('ComponentA', ComponentA)
+.component('ComponentB', ComponentB)
+.component('ComponentC', ComponentC);
 app.use(router);
 app.mount('#app');
