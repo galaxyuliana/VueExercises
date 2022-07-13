f8861650bbac255f1a178fe4701be79f1430123e
galaxyuliana <yulianashin613@gmail.com>
2022-07-12 22:48:35 -0700

    Added PropsDeclaration

--- first-vue-project/src/components/Exercise/14/Exercise14.vue
+++ first-vue-project/src/components/Exercise/14/Exercise14.vue
@@ -1,5 +1,11 @@
+<script setup>
+import PropsDeclaration from './PropsDeclaration.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 14</h2>
+        <h4>Props Declaration</h4>
+        <PropsDeclaration foo="Props Declaration"/>
     </div>
 </template>
---
+++ first-vue-project/src/components/Exercise/14/PropsDeclaration.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: ['foo'],
+  created() {
+    console.log(this.foo)
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        Check the console
+    </div>
+</template>
