630748e02238254c223ee9157c4443c751e3ea3c
galaxyuliana <yulianashin613@gmail.com>
2022-07-12 22:52:55 -0700

    Added PropsDeclarationObject

--- first-vue-project/src/components/Exercise/14/Exercise14.vue
+++ first-vue-project/src/components/Exercise/14/Exercise14.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PropsDeclaration from './PropsDeclaration.vue'
+import PropsDeclarationObject from './PropsDeclarationObject.vue'
 </script>
 
 <template>
@@ -7,5 +8,7 @@ import PropsDeclaration from './PropsDeclaration.vue'
         <h2>Exercise 14</h2>
         <h4>Props Declaration</h4>
         <PropsDeclaration foo="Props Declaration"/>
+        <h4>Props Declaration using object</h4>
+        <PropsDeclarationObject title="Props Declaration using object" likes="613"/>
     </div>
 </template>
---
+++ first-vue-project/src/components/Exercise/14/PropsDeclarationObject.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: {
+    title: String,
+    likes: Number
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <h6>{{title}}</h6>
+        {{likes}} likes.
+    </div>
+</template>
