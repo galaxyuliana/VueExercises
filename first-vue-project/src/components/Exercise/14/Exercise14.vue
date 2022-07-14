e215fc775d42698292196fa152415cfc5b1c2678
galaxyuliana <yulianashin613@gmail.com>
2022-07-13 23:43:15 -0700

    Added PropNameCasing

--- first-vue-project/src/components/Exercise/14/Exercise14.vue
+++ first-vue-project/src/components/Exercise/14/Exercise14.vue
@@ -1,6 +1,7 @@
 <script setup>
 import PropsDeclaration from './PropsDeclaration.vue'
 import PropsDeclarationObject from './PropsDeclarationObject.vue'
+import PropNameCasing from './PropNameCasing.vue'
 </script>
 
 <template>
@@ -10,5 +11,7 @@ import PropsDeclarationObject from './PropsDeclarationObject.vue'
         <PropsDeclaration foo="Props Declaration"/>
         <h4>Props Declaration using object</h4>
         <PropsDeclarationObject title="Props Declaration using object" likes="613"/>
+        <h4>Prop Name Casing</h4>
+        <PropNameCasing greeting-message="hello"/>
     </div>
 </template>
