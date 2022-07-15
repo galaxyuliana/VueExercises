aa887789c254eca3339b6bcd4721ac030a21a210
galaxyuliana <yulianashin613@gmail.com>
2022-07-14 21:36:13 -0700

    Updated Exercise14

--- first-vue-project/src/components/Exercise/14/Exercise14.vue
+++ first-vue-project/src/components/Exercise/14/Exercise14.vue
@@ -10,7 +10,7 @@ import PropNameCasing from './PropNameCasing.vue'
         <h4>Props Declaration</h4>
         <PropsDeclaration foo="Props Declaration"/>
         <h4>Props Declaration using object</h4>
-        <PropsDeclarationObject title="Props Declaration using object" likes="613"/>
+        <PropsDeclarationObject title="Props Declaration using object" :likes="613"/>
         <h4>Prop Name Casing</h4>
         <PropNameCasing greeting-message="hello"/>
     </div>
