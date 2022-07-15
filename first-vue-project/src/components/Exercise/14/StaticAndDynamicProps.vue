86acc0f9440150d1c978974b32ea721d1956a29c
galaxyuliana <yulianashin613@gmail.com>
2022-07-14 21:42:05 -0700

    Added StaticAndDynamicProps

---
+++ first-vue-project/src/components/Exercise/14/StaticAndDynamicProps.vue
@@ -0,0 +1,30 @@
+<script>
+import BlogPost from './BlogPost.vue'
+
+export default {
+  components: {
+    BlogPost
+  },
+  data() {
+    return {
+      post: {
+        id: 1,
+        title: 'My Journey with Vue',
+        author: {
+            name: 'Yuliana'
+        }
+      },
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <BlogPost title="My journey with Vue" />
+        <!-- Dynamically assign the value of a variable -->
+        <BlogPost :title="post.title" />
+        <!-- Dynamically assign the value of a complex expression -->
+        <BlogPost :title="post.title + ' by ' + post.author.name" />
+    </div>
+</template>
