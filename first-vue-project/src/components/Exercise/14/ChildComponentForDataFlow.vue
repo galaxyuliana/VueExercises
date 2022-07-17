fb2fbabcdf1a48b01e1fc352ad55203814d4e43b
galaxyuliana <yulianashin613@gmail.com>
2022-07-16 23:57:13 -0700

    Added ChildComponentForDataFlow

---
+++ first-vue-project/src/components/Exercise/14/ChildComponentForDataFlow.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        props: ['initialCounter', 'size'],
+        data() {
+            return {
+                // counter only uses this.initialCounter as the initial value;
+                // it is disconnected from future prop updates.
+                counter: this.initialCounter
+            }
+        },
+        computed: {
+            // computed property that auto-updates when the prop changes
+            normalizedSize() {
+                return this.size.trim().toLowerCase()
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="exercise">
+        initial value of Counter: {{ counter }}
+        Size: {{ normalizedSize }}
+    </div>
+</template>
