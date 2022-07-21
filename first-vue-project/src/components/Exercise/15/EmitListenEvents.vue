29689c247905246a8ab8ae9a0fbf48eccf84cb1c
galaxyuliana <yulianashin613@gmail.com>
2022-07-20 22:36:18 -0700

    Added EmitListenEvents

---
+++ first-vue-project/src/components/Exercise/15/EmitListenEvents.vue
@@ -0,0 +1,20 @@
+<script>
+import MyComponent from './MyComponent.vue'
+
+export default {
+  components: {
+    MyComponent
+  },
+  methods: {
+    callback() {
+        alert("You just clicked the button!");
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <MyComponent @submit-event="callback" />
+    </div>
+</template>
--- first-vue-project/src/components/Exercise/15/Exercise15.vue
+++ first-vue-project/src/components/Exercise/15/Exercise15.vue
@@ -1,5 +1,11 @@
+<script setup>
+import EmitListenEvents from './EmitListenEvents.vue'
+</script>
+
 <template>
     <div>
         <h2>Exercise 15</h2>
+        <h4>Emitting and Listening to Events</h4>
+        <EmitListenEvents />
     </div>
 </template>
