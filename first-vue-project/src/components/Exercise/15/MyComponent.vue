9ddb258146205c96342c04ea24e5b40d6327a93e
galaxyuliana <yulianashin613@gmail.com>
2022-07-20 22:32:27 -0700

    Added MyComponent

---
+++ first-vue-project/src/components/Exercise/15/MyComponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  methods: {
+    submit() {
+      this.$emit('submitEvent')
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="$emit('submitEvent')">click me</button>
+        <button @click="submit">click me</button>
+    </div>
+</template>
