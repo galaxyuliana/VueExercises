e16b66c79baabc154d1f21f97c4ffb62960c23bd
galaxyuliana <yulianashin613@gmail.com>
2022-07-21 23:51:14 -0700

    Added EventArguments

---
+++ first-vue-project/src/components/Exercise/15/EventArguments.vue
@@ -0,0 +1,27 @@
+<script>
+import MyButton from './MyButton.vue'
+
+export default {
+  components: {
+    MyButton
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increaseCount(n) {
+        this.count += n
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        {{ count }}
+        <MyButton @increase-by="(n) => count += n" />
+        <MyButton @increase-by="increaseCount" />
+    </div>
+</template>
--- first-vue-project/src/components/Exercise/15/Exercise15.vue
+++ first-vue-project/src/components/Exercise/15/Exercise15.vue
@@ -1,5 +1,6 @@
 <script setup>
 import EmitListenEvents from './EmitListenEvents.vue'
+import EventArguments from './EventArguments.vue'
 </script>
 
 <template>
@@ -7,5 +8,7 @@ import EmitListenEvents from './EmitListenEvents.vue'
         <h2>Exercise 15</h2>
         <h4>Emitting and Listening to Events</h4>
         <EmitListenEvents />
+        <h4>Event Arguments</h4>
+        <EventArguments />
     </div>
 </template>
