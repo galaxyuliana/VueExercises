9164a23e0ca1b13ea872432a33a56408072f9b94
galaxyuliana <yulianashin613@gmail.com>
2022-07-23 01:44:05 -0700

    Added MyForm

---
+++ first-vue-project/src/components/Exercise/15/MyForm.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+        emailAddress: '',
+        pwd: ''
+    }
+  },
+  emits: {
+    // No validation
+    click: null,
+    // Validate submit event
+    submit: ({ email, password }) => {
+      if (email && password) {
+        return true
+      } else {
+        console.warn('Invalid submit event payload!')
+        return false
+      }
+    }
+  },
+  methods: {
+    submitForm(email, password) {
+      this.$emit('submit', { email, password })
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <form>
+            <input v-model="emailAddress" placeholder="email@example.com" />
+            <input v-model="pwd" type="password" />
+            <button @click="submitForm(emailAddress, pwd)">click me</button>
+        </form>
+    </div>
+</template>
