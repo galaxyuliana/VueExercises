f66e118ab17cf0dc93e5768e3fac5611b89c8401
galaxyuliana <yulianashin613@gmail.com>
2022-07-22 23:27:09 -0700

    Added DeclareEmits

---
+++ first-vue-project/src/components/Exercise/15/DeclareEmits.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  emits: ['submitEvent']
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <button @click="$emit('submitEvent')">click me</button>
+    </div>
+</template>
