8f7736b57b5471e00058355b16deec634f270332
galaxyuliana <yulianashin613@gmail.com>
2022-07-26 23:10:31 -0700

    Added MyComponentVMArgument

---
+++ first-vue-project/src/components/Exercise/15/MyComponentVMArgument.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: ['title'],
+  emits: ['update:title']
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input
+            type="text"
+            :value="title"
+            @input="$emit('update:title', $event.target.value)"
+        />
+    </div>
+</template>
