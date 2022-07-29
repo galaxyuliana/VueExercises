935dc7156231d2396725e8e1b100dd9c15381cb8
galaxyuliana <yulianashin613@gmail.com>
2022-07-28 22:09:26 -0700

    Added UserName

---
+++ first-vue-project/src/components/Exercise/15/UserName.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+	  firstName: String,
+  	  lastName: String
+	},
+  emits: ['update:firstName', 'update:lastName']
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input
+            type="text"
+            :value="firstName"
+            @input="$emit('update:firstName', $event.target.value)"
+        />
+        <input
+            type="text"
+            :value="lastName"
+            @input="$emit('update:lastName', $event.target.value)"
+        />
+    </div>
+</template>
