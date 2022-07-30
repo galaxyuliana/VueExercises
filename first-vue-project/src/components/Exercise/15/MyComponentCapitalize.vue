6197655f12a88dad31ce855f4ae2681691437a93
galaxyuliana <yulianashin613@gmail.com>
2022-07-29 21:41:56 -0700

    Added MyComponentCapitalize

---
+++ first-vue-project/src/components/Exercise/15/MyComponentCapitalize.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    modelValue: String,
+    modelModifiers: {
+      default: () => ({})
+    }
+  },
+  emits: ['update:modelValue'],
+  methods: {
+    emitValue(e) {
+      let value = e.target.value
+      if (this.modelModifiers.capitalize) {
+        value = value.charAt(0).toUpperCase() + value.slice(1)
+      }
+      this.$emit('update:modelValue', value)
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="exercise">
+        <input type="text" :value="modelValue" @input="emitValue" />
+    </div>
+</template>
